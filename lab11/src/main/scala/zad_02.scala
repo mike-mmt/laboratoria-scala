01ad99f1686627a78b049ee1b36301b3af0c8dac
Michał Bunda <m.bunda.721@studms.ug.edu.pl>
2023-05-10 11:45:53 +0200

    lab11 zad_02

--- lab11/src/main/scala/zad_02.scala
+++ lab11/src/main/scala/zad_02.scala
@@ -1,4 +1,4 @@
-package lab11_1
+package lab11_2
 
 import akka.actor.{ActorSystem, Actor, ActorLogging, ActorRef, Props}
 
@@ -8,11 +8,12 @@ case class Graj2(przeciwnik: ActorRef, maks: Int)
 
 class Gracz01 extends Actor with ActorLogging {
   def receive: Receive = {
-    case Graj(przeciwnik) =>
+    case Graj2(przeciwnik, maks) =>
       log.info(s"serwuję do ${przeciwnik.path}")
       przeciwnik ! Piłeczka
-      context.become(gramy)
-    case Piłeczka => 
+      context.become(gramyLicznik(maks - 1))
+    case Piłeczka =>
+      log.info(s"odbijam do ${sender()}") 
       sender() ! Piłeczka
       context.become(gramy)
     case msg => log.info(s"Odebrałem wiadomość: ${msg}")
@@ -23,6 +24,17 @@ class Gracz01 extends Actor with ActorLogging {
       val przeciwnik = sender()
       log.info(s"odbijam do ${przeciwnik.path}")
       sender() ! Piłeczka
+  }
+  def gramyLicznik(pozostałoOdbić: Int): Receive = {
+    case Piłeczka =>
+      if (pozostałoOdbić > 0)
+        val przeciwnik = sender()
+        log.info(s"odbijam do ${przeciwnik.path}")
+        sender() ! Piłeczka
+        context.become(gramyLicznik(pozostałoOdbić - 1))
+      else
+        log.info("Koniec gry")
+        context.system.terminate()
   }
   // def gramy02: Receive = {
   //   case Piłeczka =>
@@ -43,10 +55,10 @@ class Gracz01 extends Actor with ActorLogging {
 //   }
 // }
 
-@main def main: Unit = {
+@main def main2: Unit = {
   val system = ActorSystem("Zawody")
     val gracz1 = system.actorOf(Props[Gracz01](), "gracz1")
     val gracz2 = system.actorOf(Props[Gracz01](), "gracz2")
     // gracz1 ! Graj(gracz2)
-    gracz1 ! Graj2(gracz2, 8)
+    gracz1 ! Graj2(gracz2, 5)
 }
